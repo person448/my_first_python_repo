443ca3a522b3f06d01bece78db460d28dcf39c9e
person448 <zoharjaipuri@gmail.com>
2025-12-20 11:58:08 -0800

    Add/update plumfile, mangofile, and exercisefile

---
+++ exercisefile.py
@@ -0,0 +1,24 @@
+import random
+random_exercise = random.randint(1, 10)
+if random_exercise == 1:
+    print('10 push-ups')
+elif random_exercise == 2:
+    print('10 squats')
+elif random_exercise == 3:
+    print('3 30 second planks')
+elif random_exercise == 4:
+    print('10 pullups')
+elif random_exercise == 5:
+    print('10 doorframe pull ups')
+elif random_exercise == 6:
+    print('3 30 second pike holds')
+elif random_exercise == 7:
+    print('10 burpees')
+elif random_exercise == 8:
+    print('2 20 second mountain climbers')
+elif random_exercise == 9:
+    print('3 30 second wall sits')
+elif random_exercise == 10:
+    print('30 second rest')
+else:
+    print('something went wrong. try again.')
---
+++ plumfile.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+exponent = int(input("Enter an exponent: "))
+
+result = number ** exponent
+
+print(f"{number} raised to the power of {exponent} is {result}")
+
