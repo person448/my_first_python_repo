3268a7be06ff1a4b19ae6c406ffa03e0d9dcbaa6
person448 <zoharjaipuri@gmail.com>
2025-12-31 17:49:41 -0800

    adding new files and deleting old ones

---
+++ deffunctions.py
@@ -0,0 +1,11 @@
+weight = input('enter weight in pounds:')
+def planet_weight(weight):
+    moon = weight * 0.165
+    mars = weight * 0.378
+    print('weight on the moon is:' , moon)
+    print('weight on mars is:' , mars)
+
+print(planet_weight(int(weight)))
+
+
+    
--- plumfile.py
+++
@@ -1,4 +0,0 @@
-num = int(input('enter number'))
-square = int(input('enter exponent of your choice'))
-power = num ** square
-print(f'{num} to the power of {square} is = {power} ')
